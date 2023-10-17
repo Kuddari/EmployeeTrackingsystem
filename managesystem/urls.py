90798bfe4a6e98ecd37e0fdc91b3282ee47c3983
Sulaiman631437005 <96813241+Sulaiman631437005@users.noreply.github.com>
2023-10-17 22:47:34 +0700

    update report formreport

--- managesystem/urls.py
+++ managesystem/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login', views.login_view, name='login'),
+    path('login/', views.login_view, name='login'),
     path('selectfilter/', views.selectfilter_view, name='selectfilter'),
     path('informations/',views.informations_view,name='informations'),
     path('formreport/',views.formreport_view,name='formreport'),

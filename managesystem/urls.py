4ae1b60d85b252f78551b38285d577b0cc5d7dfe
Sulaiman631437005 <96813241+Sulaiman631437005@users.noreply.github.com>
2023-10-23 13:15:18 +0700

    update Switch open-close sem2

--- managesystem/urls.py
+++ managesystem/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('selectfilter/', views.selectfilter_view, name='selectfilter'),
     path('informationstaff/',views.informationstaff_view,name='informationstaff'),
-    path('',views.informationuser_view,name='informationuser'),
-    path('formreport/',views.formreport_view,name='formreport'),
+    path('informationuser/',views.informationuser_view,name='informationuser'),
+    path('',views.formreport_view,name='formreport'),
     path('report/',views.report_view,name='report'),
 ]

25893305e41706f2830420056b8e47e9352b79fd
Sulaiman631437005 <96813241+Sulaiman631437005@users.noreply.github.com>
2024-02-14 14:54:05 +0700

    update

--- managesystem/admin.py
+++ managesystem/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-
+from import_export.admin import ImportExportModelAdmin
 from .models import *
 
+from .admin_resources import UserResource
+
+class UserAdmin(ImportExportModelAdmin):
+    resource_class = UserResource
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
 admin.site.register(Employee)
-admin.site.register(Work)
-admin.site.register(Setunit)
-admin.site.register(Result)
-admin.site.register(Save)
-admin.site.register(Evaluation)
+admin.site.register(Work, ImportExportModelAdmin)
+admin.site.register(Setunit, ImportExportModelAdmin)
+admin.site.register(Result, ImportExportModelAdmin)
+admin.site.register(Save, ImportExportModelAdmin)
+admin.site.register(Evaluation, ImportExportModelAdmin)
 
--- manageweb/settings.py
+++ manageweb/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "managesystem",
+    'import_export',
 ]
 
 MIDDLEWARE = [
